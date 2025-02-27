64a795e644693702f9e4bb4e132a9b2b0cb914df
matteo-974 <matteochauchet974@gmail.com>
2025-02-27 10:14:18 +0100

    v0.3.0 (Question 4)

--- src/UDPClient.java
+++ src/UDPClient.java
@@ -4,29 +4,30 @@ public class UDPClient {
     public static void main(String[] args) {
         final int SERVER_PORT = 3020; // Port du serveur
         final String SERVER_ADDRESS = "127.0.0.1"; // Adresse du serveur (localhost)
-        String message = "hello serveur";
 
         try {
             DatagramSocket clientSocket = new DatagramSocket();
+
+            // Préparer le message
+            String message = "hello serveur RX302";
+            byte[] data = message.getBytes();
             InetAddress serverAddress = InetAddress.getByName(SERVER_ADDRESS);
 
             // Envoi du message au serveur
-            byte[] data = message.getBytes();
             DatagramPacket sendPacket = new DatagramPacket(data, data.length, serverAddress, SERVER_PORT);
             clientSocket.send(sendPacket);
-            System.out.println("Message envoyé au serveur : " + message);
+            System.out.println("Message envoyé : " + message);
 
             // Réception de la réponse du serveur
             byte[] buffer = new byte[1024];
             DatagramPacket receivedPacket = new DatagramPacket(buffer, buffer.length);
             clientSocket.receive(receivedPacket);
 
-            // Affichage de la réponse
+            // Afficher la réponse
             String response = new String(receivedPacket.getData(), 0, receivedPacket.getLength());
-            System.out.println("Réponse du serveur : " + response + " : " +
-                    receivedPacket.getAddress() + ":" + receivedPacket.getPort());
+            System.out.println("Réponse reçue : " + response + " : " + receivedPacket.getAddress() + ":" + receivedPacket.getPort());
 
-            clientSocket.close();
+            clientSocket.close(); // Fermer le socket
         } catch (Exception e) {
             e.printStackTrace();
         }
--- src/UDPServer.java
+++ src/UDPServer.java
@@ -3,29 +3,35 @@ import java.net.*;
 public class UDPServer {
     public static void main(String[] args) {
         final int SERVER_PORT = 3020; // Port d'écoute du serveur
-        byte[] buffer = new byte[1024];
+        byte[] buffer = new byte[1024]; // Buffer pour recevoir les données
 
         try {
             DatagramSocket serverSocket = new DatagramSocket(SERVER_PORT);
-            System.out.println("Serveur UDP en attente sur le port " + SERVER_PORT);
+            System.out.println("Serveur UDP démarré sur le port " + SERVER_PORT);
 
             while (true) {
-                // Réception du message client
+                // Réception du message du client
                 DatagramPacket receivedPacket = new DatagramPacket(buffer, buffer.length);
                 serverSocket.receive(receivedPacket);
 
-                // Extraction et affichage des informations
-                String message = new String(receivedPacket.getData(), 0, receivedPacket.getLength());
+                // Extraire les informations du client
                 InetAddress clientAddress = receivedPacket.getAddress();
                 int clientPort = receivedPacket.getPort();
+                String message = new String(receivedPacket.getData(), 0, receivedPacket.getLength());
+
                 System.out.println("Nouveau client : " + clientAddress + ":" + clientPort);
                 System.out.println("Message reçu : " + message);
 
-                // Réponse au client
-                String response = "Serveur RX302 ready";
-                byte[] responseData = response.getBytes();
-                DatagramPacket responsePacket = new DatagramPacket(responseData, responseData.length, clientAddress, clientPort);
-                serverSocket.send(responsePacket);
+                // Vérification du message attendu
+                if (message.equals("hello serveur RX302")) {
+                    String response = "Serveur RX302 ready";
+                    byte[] responseData = response.getBytes();
+
+                    // Envoi de la réponse au client
+                    DatagramPacket responsePacket = new DatagramPacket(responseData, responseData.length, clientAddress, clientPort);
+                    serverSocket.send(responsePacket);
+                    System.out.println("Réponse envoyée : " + response);
+                }
             }
         } catch (Exception e) {
             e.printStackTrace();
